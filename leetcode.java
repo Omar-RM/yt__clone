2e9e37b05586279ccac5c444065ee310eedc7f4f
Omar Rico <83613313+omar-rm@users.noreply.github.com>
2024-08-20 13:45:10 +0100

    LeetCode No. 80

--- leetcode.java
+++ leetcode.java
@@ -75,7 +75,7 @@ class Solution {
 
        while(l1!=null||l2!=null){
 
-        int sum = 0+carry;//--------------------> Initialized sum
+        int sum = 0+carry;//--------------------> Initialize sum
         
         if(l1!=null){//-------------------------> Use number from first list
             sum += l1.val;  
@@ -96,4 +96,24 @@ class Solution {
        return result.next;
         
     }
+}
+
+// LeetCode 80.  Remove Duplicates from Sorted Array II
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length < 3) return nums.length;//----> If array is short then return length.
+            
+
+        int index = 2;//--------------------------------->  
+
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[index - 2]) {//----------> Check if current index not is equal to in value n[index-2]
+                nums[index++] = nums[i];//---------------> If not equal set value n[index+1] to current n[i] value
+            }
+        }
+
+        return index;
+    }
+
 }
