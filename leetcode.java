ae611761a87675d92ab91732c5620182a79f0374
Omar Rico <83613313+omar-rm@users.noreply.github.com>
2024-08-20 13:30:37 +0100

    LeetCode No 2

--- leetcode.java
+++ leetcode.java
@@ -63,3 +63,37 @@ class Solution {
         return maxLength;
     }
 }
+
+
+// LeetCode No 2
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+       ListNode result = new ListNode();
+       ListNode ptr = result;
+
+       int carry = 0;//-------------------------> Set default carry to 0
+
+       while(l1!=null||l2!=null){
+
+        int sum = 0+carry;//--------------------> Initialized sum
+        
+        if(l1!=null){//-------------------------> Use number from first list
+            sum += l1.val;  
+            l1=l1.next;
+        }
+        if(l2!=null){//-------------------------> Use number from second list
+            sum += l2.val;
+            l2=l2.next;
+        }
+
+        carry =  sum/10;//----------------------> Get carry digit number and sum digit
+        sum = sum%10;
+        ptr.next = new ListNode(sum);//---------> Add sum current digit to NodeList
+        ptr = ptr.next;//-----------------------> Point current node to next empty Node.
+       }
+       if(carry==1) ptr.next = new ListNode(1);
+
+       return result.next;
+        
+    }
+}
